0dfb66e25a823e45b9afef9f5103a43c25e91903
PJDEEPESH <167789969+PJDEEPESH@users.noreply.github.com>
2024-06-11 22:07:10 +0530

    Add files via upload

---
+++ Asciivalueofstring.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class  Asciivalueofstring
+{
+    public static void main(String [] args)
+    {
+        System.out.print("Enter the string for what you want the value of the ascii : ");
+        Scanner scan = new Scanner(System.in);
+        String string = scan.nextLine();
+        value(string);
+    }
+    public static void value(String string)
+    {
+        int score = 0;
+        for(int i = 0;i<string.length();i++)
+        {
+            score = score + string.charAt(i);
+        }
+        System.out.print("The value of the string is = "+ score+".");
+    }
+}
