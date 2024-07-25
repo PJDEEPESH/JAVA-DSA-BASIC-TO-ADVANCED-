23fe607971d27324ce9ab31c69755b30a98782fc
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-07-25 12:36:51 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,12 @@
+class primitivepublic 
+{
+    public static void main(String[] args) 
+    {
+        int a = 299;
+        long b = 87497394797L;
+        float c = 94.93f;
+        double d = 837973734.97398;
+        char e = 'D';
+        boolean f = true;
+    }
+}
