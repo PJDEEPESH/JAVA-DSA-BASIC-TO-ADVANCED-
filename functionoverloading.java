4380df47d9b83988fb910ad991dd0812f1f06bac
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-08-05 18:36:35 +0530

    Create functionoverloading.java

---
+++ functionoverloading.java
@@ -0,0 +1,16 @@
+class functionoverloading 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(sum(1,2));
+        System.out.println(sum(4,3,2));   
+    }    
+    static int sum(int a ,int b)
+    {
+        return a + b;
+    }
+    static int sum(int a ,int b,int c)
+    {
+        return a + b + c;
+    }
+}
