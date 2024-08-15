5a6763c64ddd5526040e1c785520bfcdb2e2fffc
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-08-15 20:29:18 +0530

    Create LuckyNumberInAMatrix.java

---
+++ Arrays/LuckyNumberInAMatrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0;i<matrix.length;i++)
+          {
+            for(int j = 0 ;j<matrix[i].length;j++)
+              {
+                  int element = matrix[i][j];
+                  int min = matrix[i][j];
+                  int max = matrix[i][j];
+                  for(int k = 0;k<matrix[i].length;k++)
+                    {
+                      min = Math.min(min,matrix[i][k]);
+                    }
+                  for(int l =0 ;l<matrix.length;l++)
+                    {
+                      max = Math.max(max,matrix[l][j];
+                    }
+                if(min == element && max == element)
+                {
+                  ans.add(element);
+                }
+              }
+          }
+      return ans;
+    }
+}
