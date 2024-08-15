7fe7267fc87296c72b2636848b98c8918018944c
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-08-15 09:35:11 +0530

    Update LemonadeChange.java

--- Arrays/LemonadeChange.java
+++ Arrays/LemonadeChange.java
@@ -5,38 +5,37 @@ class Solution
         int five = 0;
         int ten = 0;
         for(int bill : bills)
+        {
+            if(bill == 5)
             {
-                if(bill === 5)
+                five++;
+            }
+            else if(bill == 10)
+            {
+                if(five == 0)
                 {
-                    five++;
+                    return false;
                 }
-                else if(bill == 10)
+                five--;
+                ten++;
+            }
+            else
+            {
+                if(ten >0 && five >0)
                 {
-                    if(five == 0)
-                    {
-                        return false;
-                    }
                     five--;
-                    ten++;
+                    ten--;
+                }
+                else if(five>=3)
+                {
+                    five -= 3;
                 }
                 else
                 {
-                    if(ten >0 && five > 0)
-                    {
-                        five--;
-                        ten--;
-                    }
-                    else if(five >= 3)
-                    {
-                        five -= 3;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
+        }
         return true;
-            
     }
 }
