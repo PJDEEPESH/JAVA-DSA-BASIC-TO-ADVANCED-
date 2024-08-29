a538a270e63ba852c68dc6ea417e671721a1ad3c
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-08-29 16:38:16 +0530

    Update searchrange.java

--- Binary Search/searchrange.java
+++ Binary Search/searchrange.java
@@ -4,50 +4,44 @@ class Solution
     {
         int start = 0;
         int end = nums.length - 1;
-        int[] ans = {-1, -1};
-
-        // Find the first occurrence
+        int[] ans = {-1,-1};
         while(start <= end)
         {
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
+            int mid = start + (end - start)/2;
+            if(target == nums[mid])
             {
                 ans[0] = mid;
-                end = mid - 1; // Continue searching in the left half
+                end = mid - 1;
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
             }
             else if(nums[mid] < target)
             {
                 start = mid + 1;
             }
-            else
-            {
-                end = mid - 1;
-            }
         }
-
-        // Reset start and end for finding the last occurrence
+        //initialize again to search in the right hand side;
         start = 0;
         end = nums.length - 1;
-
-        // Find the last occurrence
         while(start <= end)
         {
-            int mid = start + (end - start) / 2;
-            if(nums[mid] == target)
+            int mid = start + (end - start)/2;
+            if(target == nums[mid])
             {
                 ans[1] = mid;
-                start = mid + 1; // Continue searching in the right half
+                start = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
+                end = mid - 1;
             }
             else if(nums[mid] < target)
             {
                 start = mid + 1;
             }
-            else
-            {
-                end = mid - 1;
-            }
         }
-
         return ans;
     }
 }
