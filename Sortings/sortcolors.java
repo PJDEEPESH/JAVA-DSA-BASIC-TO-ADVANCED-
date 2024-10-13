10176719c0804ea0a0601fc9ac110a33f1a0f43c
Deepesh <167789969+PJDEEPESH@users.noreply.github.com>
2024-10-13 13:15:20 +0530

    Create sortcolors.java

---
+++ Sortings/sortcolors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortColors(int[] nums) {
+        for(int i = 0;i<nums.length;i++)
+        {
+            for(int j = 1;j<nums.length - i;j++)
+            {
+                if(nums[j] < nums[j-1])
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
